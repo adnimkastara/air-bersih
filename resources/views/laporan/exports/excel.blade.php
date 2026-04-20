e8b8cd85d4ab76c30f682b2600fdfb9884f2b2a0
adnimkastara <adnimkastara@gmail.com>
2026-04-20 19:13:15 +0700

    Rapikan template PDF laporan dan kuatkan metadata export

--- resources/views/laporan/exports/excel.blade.php
+++ resources/views/laporan/exports/excel.blade.php
@@ -1,10 +1,21 @@
+@php
+    $dateFrom = data_get($filters, 'date_from');
+    $dateTo = data_get($filters, 'date_to');
+    $desaId = data_get($filters, 'desa_id');
+    $reportType = data_get($filters, 'report', $report ?? null);
+    $desaFilterLabel = $desaId ? 'Desa ID '.$desaId : 'Semua Desa';
+
+    if ($reportType === 'setoran_kecamatan' && ! $desaId) {
+        $desaFilterLabel = 'Semua Desa (Laporan Kecamatan)';
+    }
+@endphp
 <table border="1">
     <thead>
     <tr>
         <th colspan="20">Laporan {{ str($report)->replace('_', ' ')->title() }}</th>
     </tr>
     <tr>
-        <th colspan="20">Filter tanggal: {{ $filters['date_from'] ?? '-' }} s/d {{ $filters['date_to'] ?? '-' }}, Desa: {{ $filters['desa_id'] ?? 'Semua' }}</th>
+        <th colspan="20">Filter tanggal: {{ $dateFrom ?? '-' }} s/d {{ $dateTo ?? '-' }}, Desa: {{ $desaFilterLabel }}</th>
     </tr>
     <tr>
         @foreach(array_keys($rows[0] ?? ['data' => 'Tidak ada data']) as $column)
